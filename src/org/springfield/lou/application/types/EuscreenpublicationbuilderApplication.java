4a5d113da642953ab6918d6dc46c0df77d682510
DavidAmmeraal <david.ammeraal@gmail.com>
2015-10-30 12:59:14 +0100

    Fixed conflicts

--- src/org/springfield/lou/application/types/EuscreenpublicationbuilderApplication.java
+++ src/org/springfield/lou/application/types/EuscreenpublicationbuilderApplication.java
@@ -106,11 +106,7 @@ public class EuscreenpublicationbuilderApplication extends Html5Application{
      	int cnt_bookmark = 0;
      	for (Bookmark bmi : bookmarks.getBookmarklist()) {
      		String id = "bookmark_"+ cnt_bookmark;
-<<<<<<< HEAD
-    		bookmarkLayout += "<div id=\"" + id +"\" class=\"drag_bookmark\"><video poster='"+bmi.getScreenshot()+"' controls><source src='"+bmi.getVideo()+"' type=\"video/mp4\"></video></div>";
-=======
     		bookmarkLayout += "<div id=\"" + id +"\" class=\"drag_bookmark\"><video  poster='"+bmi.getScreenshot()+"' src=\"" + bmi.getVideo() + "\" controls></video></div>";
->>>>>>> prod-26102015
     		bookmarkLayout += "<script type=\"text/javascript\">"
     				+ "eddie.getComponent('embedlib').loaded().then(function(){"
     				+ "		EuScreen.getVideo({src: '" + bmi.getVideo() + "', poster: '" + bmi.getScreenshot() + "', controls: true}, function(html){"
@@ -130,10 +126,7 @@ public class EuscreenpublicationbuilderApplication extends Html5Application{
      		bookmarkLayout += "<div class=\"right-header\" id='" + right_header_div_id + "'>" + col.getName() + "</div>";
      		bookmarkLayout += "<div id='" + right_toggle_div_id + "' class=\"tgl\">";
      		for (Bookmark bk : col.getVideos()) {
-<<<<<<< HEAD
-        		bookmarkLayout += "<div id=\"bookmark_"+ cnt_bookmark +"\" class=\"drag_bookmark\"><video poster='"+bk.getScreenshot()+"' controls><source src='"+bk.getVideo()+"' type=\"video/mp4\"></video></div>";
-        		cnt_bookmark++;
-=======
+
      			try{
 	     			String id = "bookmark_" + cnt_bookmark;
 	     			String src = bk.getVideo();
@@ -152,7 +145,6 @@ public class EuscreenpublicationbuilderApplication extends Html5Application{
      				e.printStackTrace();
      			}
         		
->>>>>>> prod-26102015
 			}
      		bookmarkLayout += "</div>";
      		cnt_header++;
